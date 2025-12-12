f7b1aac3ab1173aabbc62f1bfd20dea7103bdcd3
chaitanyagohar <chaitanyagohar@gmail.com>
2025-12-12 12:08:38 +0530

    dffS

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1373300017703771');
+              fbq('init', '857766096772105');
               fbq('track', 'PageView');
             `,
           }}
@@ -44,7 +44,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1373300017703771&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=857766096772105&ev=PageView&noscript=1"
             alt="Meta Pixel"
           />
         </noscript>
